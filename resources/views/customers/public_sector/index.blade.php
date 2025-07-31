97e80f3d1624f0318322ae0bc28e49fe22f2e6e0
Habil455 <raphaelhabil09@gmail.com>
2025-07-31 19:57:13 +0300

    feat: replicating SMS resend to public customers

--- resources/views/customers/public_sector/index.blade.php
+++ resources/views/customers/public_sector/index.blade.php
@@ -20,130 +20,210 @@
 
                 {{-- @can('add-customer') --}}
                 <div class="row d-flex justify-content-end">
-                <div>
-                    <a href="{{ route('public.customer.create') }}" class="btn btn-secondary btn-sm">
+                    <div>
+                        <a href="{{ route('public.customer.create') }}" class="btn btn-secondary btn-sm">
 
-                        <i class="ph-plus me-2"></i> Public Sector</a>
-                    </button>
+                            <i class="ph-plus me-2"></i> Public Sector</a>
+                        </button>
 
+                    </div>
+                    {{-- @endcan --}}
                 </div>
-                {{-- @endcan --}}
+            </div>
+
+            <ul class="nav nav-tabs mb-3 px-2" role="tablist">
+                <li class="nav-item" role="presentation">
+                    <a href="#my_customers" class="nav-link active " data-bs-toggle="tab" aria-selected="true"
+                        role="tab" tabindex="-1">
+                        My Customers
+                        <span class="badge bg-dark text-brand-secondary rounded-pill ms-2"></span>
+                    </a>
+                </li>
+            </ul>
+
+            @if (session('msg'))
+                <div class="alert alert-success mt-1 mb-1 col-10 mx-auto" role="alert">
+                    {{ session('msg') }}
+                </div>
+            @endif
+            <div class="tab-content">
+                <div class="tab-pane fade active show" id="my_customers" role="tabpanel">
+                    <table class="table table-striped table-bordered datatable-basic">
+                        <thead>
+                            <th>No.</th>
+                            <th>Name</th>
+                            <th>Phone</th>
+                            <th>Location</th>
+                            <th>Registered By</th>
+                            <th>Date</th>
+                            <th>Options</th>
+                        </thead>
+
+                        <tbody>
+                            @foreach ($customers as $customer)
+                                @php
+                                    $registered_by = App\Models\User::where('id', $customer->created_by)->first();
+                                @endphp
+                                <tr>
+                                    <td>{{ $loop->iteration }}</td>
+                                    <td>
+                                        <b>{{ $customer->first_name }} {{ $customer->last_name }}</b> <br />
+                                    </td>
+                                    <td>{{ $customer->phone_number }}</td>
+                                    <td>{{ $customer->region->name }}</td>
+                                    <td>{{ $registered_by->fname }} {{ $registered_by->lname }}</td>
+                                    <td>{{ $customer->created_at->format('d/m/Y') }}</td>
+                                    <td>
+                                        <a href="{{ route('customer.show', $customer->id) }}" title="View Info"
+                                            class="btn btn-sm btn-secondary edit-button enabled">
+                                            <i class="ph ph-info"></i>
+                                        </a>
+
+                                        @can('delete-customer')
+                                            <button type="button" class="btn btn-sm btn-danger"
+                                                onclick="deleteCustomer({{ $customer->id }})">
+                                                Delete
+                                            </button>
+                                        @endcan
+
+                                        @can('resend-credentials')
+                                            <button type="button" title="Resend Login Credentials"
+                                                onclick="resendCredentials({{ $customer->id }})"
+                                                class="btn btn-outline-warning btn-sm rounded-pill">
+                                                <i class="ph-repeat"></i>
+                                            </button>
+                                        @endcan
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
+        <script>
+            function deleteCustomer(id) {
+                console.log('Habil');
 
-        <ul class="nav nav-tabs mb-3 px-2" role="tablist">
-            <li class="nav-item" role="presentation">
-                <a href="#my_customers" class="nav-link active " data-bs-toggle="tab" aria-selected="true" role="tab"
-                    tabindex="-1">
-                    My Customers
-                    <span class="badge bg-dark text-brand-secondary rounded-pill ms-2"></span>
-                </a>
-            </li>
-        </ul>
+                Swal.fire({
+                    text: 'Are You Sure You Want to Delete This Customer ?',
+                    // text: "You won't be able to revert this!",
+                    icon: 'warning',
+                    showCancelButton: true,
+                    confirmButtonColor: '#3085d6',
+                    cancelButtonColor: '#d33',
+                    confirmButtonText: 'Yes,Delete !'
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        var terminationid = id;
 
-        @if (session('msg'))
-            <div class="alert alert-success mt-1 mb-1 col-10 mx-auto" role="alert">
-                {{ session('msg') }}
-            </div>
-        @endif
-        <div class="tab-content">
-            <div class="tab-pane fade active show" id="my_customers" role="tabpanel">
-                <table class="table table-striped table-bordered datatable-basic">
-                    <thead>
-                        <th>No.</th>
-                        <th>Name</th>
-                        <th>Phone</th>
-                        <th>Location</th>
-                        <th>Registered By</th>
-                        <th>Date</th>
-                        <th>Options</th>
-                    </thead>
+                        $.ajax({
+                                url: "" + terminationid
+                            })
+                            .done(function(data) {
+                                $('#resultfeedOvertime').fadeOut('fast', function() {
+                                    $('#resultfeedOvertime').fadeIn('fast').html(data);
+                                });
 
-                    <tbody>
-                        @foreach ($customers as $customer)
-                            @php
-                                $registered_by = App\Models\User::where('id', $customer->created_by)->first();
-                            @endphp
-                            <tr>
-                                <td>{{ $loop->iteration }}</td>
-                                <td>
-                                    <b>{{ $customer->first_name }} {{ $customer->last_name }}</b> <br />
-                                </td>
-                                <td>{{ $customer->phone_number }}</td>
-                                <td>{{ $customer->region->name }}</td>
-                                <td>{{ $registered_by->fname }} {{ $registered_by->lname }}</td>
-                                <td>{{ $customer->created_at->format('d/m/Y') }}</td>
-                                <td>
-                                    <a href="{{ route('customer.show', $customer->id) }}" title="View Info"
-                                        class="btn btn-sm btn-secondary edit-button enabled">
-                                        <i class="ph ph-info"></i>
-                                    </a>
+                                $('#status' + id).fadeOut('fast', function() {
+                                    $('#status' + id).fadeIn('fast').html(
+                                        '<div class="col-md-12"><span class="label label-warning">DELETED</span></div>'
+                                    );
+                                });
 
-                                    @can('delete-customer')
-                                    <button type="button" class="btn btn-sm btn-danger"
-                                        onclick="deleteCustomer({{ $customer->id }})">
-                                        Delete
-                                    </button>
-                                    @endcan
-                                </td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    </div>
-    <script>
-        function deleteCustomer(id) {
-            console.log('Habil');
+                                // alert('Request Cancelled Successifully!! ...');
 
+                                Swal.fire(
+                                    'Deleted !',
+                                    'Customer was Deleted Successifully!!.',
+                                    'success'
+                                )
+
+                                setTimeout(function() {
+                                    location.reload();
+                                }, 1000);
+                            })
+                            .fail(function() {
+                                Swal.fire(
+                                    'Failed!',
+                                    'Customer Deletion Failed!! ....',
+                                    'success'
+                                )
+                            });
+                    }
+                });
+            }
+
+            function resendCredentials(id) {
             Swal.fire({
-                text: 'Are You Sure You Want to Delete This Customer ?',
-                // text: "You won't be able to revert this!",
-                icon: 'warning',
+                title: 'Resend Credentials?',
+                text: 'Are you sure you want to resend login credentials to this customer via SMS?',
+                icon: 'question',
                 showCancelButton: true,
-                confirmButtonColor: '#3085d6',
-                cancelButtonColor: '#d33',
-                confirmButtonText: 'Yes,Delete !'
+                confirmButtonColor: '#ffc107',
+                cancelButtonColor: '#6c757d',
+                confirmButtonText: '<i class="ph-paper-plane-tilt me-1"></i>Yes, Send!',
+                cancelButtonText: '<i class="ph-x me-1"></i>Cancel',
+                customClass: {
+                    popup: 'shadow-lg',
+                    confirmButton: 'btn-lg',
+                    cancelButton: 'btn-lg'
+                },
+                showLoaderOnConfirm: true,
+                preConfirm: () => {
+                    return fetch("{{ route('customer.resend_sms_credentials', ':id') }}".replace(':id', id), {
+                        method: 'GET',
+                        headers: {
+                            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content'),
+                            'Accept': 'application/json',
+                            'Content-Type': 'application/json'
+                        }
+                    }).then(response => {
+                        if (!response.ok) {
+                            throw new Error('Network response was not ok');
+                        }
+                        return response.json();
+                    });
+                },
+                allowOutsideClick: () => !Swal.isLoading()
             }).then((result) => {
                 if (result.isConfirmed) {
-                    var terminationid = id;
+                    Swal.fire({
+                        title: 'Credentials Sent!',
+                        text: 'Login credentials have been sent to the customer via SMS successfully.',
+                        icon: 'success',
+                        customClass: {
+                            popup: 'shadow-lg'
+                        },
+                        timer: 3000,
+                        showConfirmButton: false
+                    });
 
-                    $.ajax({
-                            url: "" + terminationid
-                        })
-                        .done(function(data) {
-                            $('#resultfeedOvertime').fadeOut('fast', function() {
-                                $('#resultfeedOvertime').fadeIn('fast').html(data);
-                            });
-
-                            $('#status' + id).fadeOut('fast', function() {
-                                $('#status' + id).fadeIn('fast').html(
-                                    '<div class="col-md-12"><span class="label label-warning">DELETED</span></div>'
-                                );
-                            });
-
-                            // alert('Request Cancelled Successifully!! ...');
-
-                            Swal.fire(
-                                'Deleted !',
-                                'Customer was Deleted Successifully!!.',
-                                'success'
-                            )
-
-                            setTimeout(function() {
-                                location.reload();
-                            }, 1000);
-                        })
-                        .fail(function() {
-                            Swal.fire(
-                                'Failed!',
-                                'Customer Deletion Failed!! ....',
-                                'success'
-                            )
+                    // Add temporary success highlight to the row
+                    $(`tr:has(button[onclick="resendCredentials(${id})"])`).addClass('table-success').delay(2000)
+                        .queue(function() {
+                            $(this).removeClass('table-success').dequeue();
                         });
+                } else if (result.isDenied) {
+                    Swal.fire({
+                        title: 'Failed!',
+                        text: 'Failed to send credentials. Please try again.',
+                        icon: 'error',
+                        customClass: {
+                            popup: 'shadow-lg'
+                        }
+                    });
                 }
+            }).catch((error) => {
+                Swal.fire({
+                    title: 'Error!',
+                    text: 'An error occurred while sending credentials. Please try again.',
+                    icon: 'error',
+                    customClass: {
+                        popup: 'shadow-lg'
+                    }
+                });
             });
         }
     </script>
-@endsection
+    @endsection
