e54feffd1a82061f967e34e951d98c423c16fd47
Habil455 <raphaelhabil09@gmail.com>
2025-08-09 11:13:24 +0300

    fux: show beneficiaries

--- resources/views/customers/beneficiaries/show.blade.php
+++ resources/views/customers/beneficiaries/show.blade.php
@@ -2,7 +2,7 @@
     <div class="modal-dialog modal-lg">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title">Large modal</h5>
+                <h5 class="modal-title">BENEFICIARIES</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
             </div>
 
